ab6a5d10c0c3446dbd07fb1bdf643499dabbd1d2
Abdullah Al Marzan <marzansust16@gmail.com>
2025-09-04 10:30:14 +0600

    update

--- codes/Version 4/s0f_consolidate_affinity.py
+++ codes/Version 4/s0f_consolidate_affinity.py
@@ -79,7 +79,7 @@ def consolidate_affinity_scores():
     
     # Define the percentiles you want to calculate
     #percentiles_to_calc = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95]
-    percentiles_to_calc = [0.25, 0.75]
+    #percentiles_to_calc = [0.25, 0.75]
     
     # Create a dictionary of aggregation functions
     aggregations = {
