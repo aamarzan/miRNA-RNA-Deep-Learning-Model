045c667c3bd05d8368719303470f8c2c356a4b23
Abdullah Al Marzan <marzansust16@gmail.com>
2025-09-26 23:14:32 +0600

    update

--- codes/Version 4/python resume_training_with_profiling.py
+++ codes/Version 4/python resume_training_with_profiling.py
@@ -61,12 +61,12 @@ resume_callbacks = [
 ]
 
 # --- Resume training ---
-print("\nResuming training from Epoch 50...")
+print("\nResuming training from Epoch 51...")
 model.fit(
     train_gen,
     validation_data=test_gen,
     epochs=params['epochs'],
-    initial_epoch=50,
+    initial_epoch=51,
     callbacks=resume_callbacks,
     verbose=1
 )
